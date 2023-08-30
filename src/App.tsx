0795ef149f8c9ffeb70bd1d0f9ac59405493f5c2
Kitcht <oteamton@gmail.com>
2023-08-30 16:30:23 +0700

    v0.0.4

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home'
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
