ca975660eef8192b24ba2c5784f180c6b03f8851
Kitcht <oteamton@gmail.com>
2023-09-01 07:46:04 +0700

    v0.0.3 decorating

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles/Dashboard.css';
 
 const Dashboard: React.FC = () => {
+  const [boxes, setBoxes] = useState([{ id: 1 }]);
+
+  const handleAddDeviceClick = () => {
+    const newBoxId = boxes.length + 1;
+    setBoxes([...boxes, { id: newBoxId }]);
+  };
+
   return (
-    <div>
-      <h1>Welcome to the Dashboard!</h1>
-      {/* Add your dashboard components here */}
+    <div className="dashboard-container">
+      <h1 className="dashboard-title">Welcome to the Dashboard!</h1>
+      <div className="dashboard-section">
+        {boxes.map((box) => (
+          <div key={box.id} className="dashboard-box">
+            <h2>Box {box.id}</h2>
+            {/* Modify content in box */}
+          </div>
+        ))}
+        <button className="add-device-button" onClick={handleAddDeviceClick}>
+          Add Device
+        </button>
+      </div>
     </div>
   );
 };
